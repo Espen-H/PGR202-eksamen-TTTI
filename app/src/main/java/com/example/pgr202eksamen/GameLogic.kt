f7d6927509d07e5a29f691b1ded78014e34870c2
unknown <espen.hoines@gmail.com>
2019-08-07 21:13:52 +0200

    fleshed out the game logic

--- app/src/main/java/com/example/pgr202eksamen/GameLogic.kt
+++ app/src/main/java/com/example/pgr202eksamen/GameLogic.kt
@@ -1,11 +1,62 @@
 package com.example.pgr202eksamen
 
-class GameLogic() {
+class GameLogic(aiGame: Boolean) {
 
     private val player1: MutableList<Int> = emptyArray<Int>().toMutableList()
     private val player2: MutableList<Int> = emptyArray<Int>().toMutableList()
+    private var player1TurnBoolean: Boolean = true
+    private var aiPlayer2: Boolean = aiGame
+    private var turnCounter = 0
 
-    fun checkWinner() {
+    private lateinit var board: Array<IntArray>
+    private lateinit var moveHistory: MutableList<Int>
+
+    private fun setupBoard() {
+        board = Array(4) { IntArray(4) }
+        moveHistory = emptyArray<Int>().toMutableList()
+        turnCounter = 0
+
+        //Random player starts
+        when ((0..1).random()) {
+            0 -> player1TurnBoolean = true
+            1 -> player1TurnBoolean = false
+        }
+        var numId = 0
+        for (i in 0 until board.size) {
+            val colArray = IntArray(4)
+
+            for (j in 0 until colArray.size) {
+                colArray[j] = numId++
+            }
+            board[i] = colArray
+        }
+    }
+
+    /*
+            Restricting players from choosing the same box will be done with the onClick event on the boxes
+     */
+
+    private fun play(boxId: Int) {
+        when (moveHistory.contains(boxId)) {
+            true -> return illegalMove()
+            false ->
+                when (player1TurnBoolean) {
+                    true -> player1.add(boxId)
+                    false -> player2.add(boxId)
+                }
+        }
+        moveHistory.add(boxId)
+    }
+
+    private fun illegalMove() {
+        /*
+         #TODO
+         Make Toast telling the player that the move was against the rules
+       */
+
+    }
+
+    private fun checkWinner() {
         var winner = -1
         val winningLines: Collection<IntArray> =
             mutableListOf(
@@ -27,9 +78,11 @@ class GameLogic() {
         for (line in winningLines) {
             if (player1.containsAll(line.toMutableList())) {
                 winner = 1
+                return // Tell the UI that game is over
             }
             if (player2.containsAll(line.toMutableList())) {
                 winner = 2
+                return // Tell the UI that game is over
 
             }
         }
